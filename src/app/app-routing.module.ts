caef783a031d31f4b6d9b0741d4f98a4ae60d5ed
jacksontrr <jackson.torres.1000@gmail.com>
2021-02-25 16:17:24 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,62 +2,78 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
   {
     path: '',
     redirectTo: 'inicial',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    pathMatch: 'full',
   },
   {
     path: 'inicial',
-    loadChildren: () => import('./pages/inicial/inicial.module').then( m => m.InicialPageModule)
+    loadChildren: () =>
+      import('./pages/inicial/inicial.module').then((m) => m.InicialPageModule),
   },
   {
     path: 'cadastrar-prof',
-    loadChildren: () => import('./pages/cadastrar-prof/cadastrar-prof.module').then( m => m.CadastrarProfPageModule)
+    loadChildren: () =>
+      import('./pages/cadastrar-prof/cadastrar-prof.module').then(
+        (m) => m.CadastrarProfPageModule
+      ),
   },
   {
     path: 'cadastrar-cli',
-    loadChildren: () => import('./pages/cadastrar-cli/cadastrar-cli.module').then( m => m.CadastrarCliPageModule)
+    loadChildren: () =>
+      import('./pages/cadastrar-cli/cadastrar-cli.module').then(
+        (m) => m.CadastrarCliPageModule
+      ),
   },
   {
     path: 'login-prof',
-    loadChildren: () => import('./pages/login-prof/login-prof.module').then( m => m.LoginProfPageModule)
+    loadChildren: () =>
+      import('./pages/login-prof/login-prof.module').then(
+        (m) => m.LoginProfPageModule
+      ),
   },
   {
     path: 'login-cli',
-    loadChildren: () => import('./pages/login-cli/login-cli.module').then( m => m.LoginCliPageModule)
+    loadChildren: () =>
+      import('./pages/login-cli/login-cli.module').then(
+        (m) => m.LoginCliPageModule
+      ),
   },
   {
     path: 'formulario-prof',
-    loadChildren: () => import('./pages/formulario-prof/formulario-prof.module').then( m => m.FormularioProfPageModule)
+    loadChildren: () =>
+      import('./pages/formulario-prof/formulario-prof.module').then(
+        (m) => m.FormularioProfPageModule
+      ),
   },
   {
     path: 'formulario-cli',
-    loadChildren: () => import('./pages/formulario-cli/formulario-cli.module').then( m => m.FormularioCliPageModule)
+    loadChildren: () =>
+      import('./pages/formulario-cli/formulario-cli.module').then(
+        (m) => m.FormularioCliPageModule
+      ),
   },
   {
     path: 'home-prof',
-    loadChildren: () => import('./pages/home-prof/home-prof.module').then( m => m.HomeProfPageModule)
+    loadChildren: () =>
+      import('./pages/home-prof/home-prof.module').then(
+        (m) => m.HomeProfPageModule
+      ),
   },
   {
     path: 'home-cli',
-    loadChildren: () => import('./pages/home-cli/home-cli.module').then( m => m.HomeCliPageModule)
+    loadChildren: () =>
+      import('./pages/home-cli/home-cli.module').then(
+        (m) => m.HomeCliPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
